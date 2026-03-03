2a76816aa651f2cd7b83f11c71ceaa338a84f0ba
Ryu-GY <a56322505@gmail.com>
2026-03-03 18:33:16 +0900

    🌱 feat:      유저모델 수정

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -7,8 +7,7 @@ from tortoise.models import Model
 class User(Model):
     id = fields.IntField(pk=True)
     kakao_id = fields.CharField(max_length=50, unique=True, null=True)
-    nickname = fields.CharField(max_length=20)
-    hashed_password = fields.CharField(max_length=256)
+    nickname = fields.CharField(max_length=20, unique=True)
     profile_image_url = fields.TextField(null=True)
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
